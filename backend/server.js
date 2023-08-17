f943187c34721eb23939f6db69ded912e2c07195
Achyuth-Prime <achyuthprime@gmail.com>
2023-08-17 09:00:51 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ const app=express();
 // Middleware
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false})); 
-app.use(
-    cors({
-      origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
-    })
-  );  
+app.use(cors());  
 app.use("/api/tasks",taskRoutes); 
 
 const PORT=process.env.PORT || 5000
