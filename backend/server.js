d92b671c154957a0260745c728c897fd26b173fb
Achyuth-Prime <achyuthprime@gmail.com>
2023-08-17 09:36:14 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,11 @@ const app=express();
 // Middleware
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false})); 
-app.use(cors({
-    origin: 'https://mern-task-app.onrender.com',
-    headers:["Content-Type"],
-    credentials:true
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    // allowedHeaders: ['Content-Type', 'Authorization']
-  }));  
-// app.options('*',cors());
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
+    })
+  );
 app.use("/api/tasks",taskRoutes); 
 
 const PORT=process.env.PORT || 5000
