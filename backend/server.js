af637b5df7e0b500c215e0661b621ff1966c2a25
Achyuth-Prime <achyuthprime@gmail.com>
2023-08-18 08:40:04 +0530

    checking

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false})); 
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://mern-task-app.onrender.com","http://mern-task-app.onrender.com", "http://mern-task-app-9wxc.onrender.com","https://mern-task-app-9wxc.onrender.com"],
+      origin: ["http://localhost:3000", "https://mern-task-app.onrender.com","http://mern-task-app.onrender.com"],
       credentials: true
     })
   );
